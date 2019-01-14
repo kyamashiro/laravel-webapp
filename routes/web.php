4b3433f026f021f1ae6aec8684989c7684d06b23
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-14 18:29:52 +0900

    php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
